b4bc5b16cccb3de91aa48cdbd007b24f8dcc0c29
Alexey <Shiry-1980@mail.ru>
2023-02-02 16:33:36 +0300

    task

---
+++ task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] {"git", "333", "hello", "world", "2"};
+string[] array2 = new string[array1.Length];
+void ElementaryArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void FinalArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ElementaryArray(array1, array2);
+FinalArray(array2);
